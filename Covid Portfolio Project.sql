30e5294c5936237230207fb12634b563246d7c28
Ariane Shokri <94076142+AriaCS@users.noreply.github.com>
2022-04-05 17:20:56 -0400

    Update Covid Portfolio Project.sql

--- Covid Portfolio Project.sql
+++ Covid Portfolio Project.sql
@@ -1,74 +1,42 @@
-SELECT *
-FROM PortfolioProject..CovidDeaths
-WHERE continent is not null
-ORDER BY 3,4
+-- DATA SOURCE: Published online at OurWorldInData.org Retrieved from: https://ourworldindata.org/coronavirus (online resource)
+-- Queries used for Tableau Project
 
-SELECT *
-FROM PortfolioProject..CovidVaccinations
-ORDER BY 3,4
 
--- Select data that we will be using
-SELECT location, date, total_cases, new_cases, total_deaths, population
-FROM PortfolioProject..CovidDeaths
-ORDER BY 1,2
+-- 1. Global Numbers - double checking global numbers based off total_cases, total_deaths, and global death_percentage
 
--- Looking at Total Cases vs Total Deaths
--- Shows percent likelihood of dying from covid in selected country
-SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
-FROM PortfolioProject..CovidDeaths
-WHERE location like '%states%'
-ORDER BY 1,2
-
--- Looking at Total Cases vs Population
--- Shows what percentage of population contracted covid
-SELECT location, date, population, total_cases, (total_cases/population)*100 as percent_population_infected
-FROM PortfolioProject..CovidDeaths
-WHERE location like '%states%'
-ORDER BY 1,2
-
--- Looking at countries with highest infection rate compared to population
-SELECT location, population, MAX(total_cases) as highest_infection_count, MAX((total_cases/population))*100 as percent_population_infected
-FROM PortfolioProject..CovidDeaths
-WHERE continent is not null
-GROUP BY location, population
-ORDER BY percent_population_infected desc
-
--- Continents with the highest death count per population
-SELECT continent, MAX(cast(total_deaths as int)) as total_death_count
-FROM PortfolioProject..CovidDeaths
-WHERE continent is not null
-GROUP BY continent
-ORDER BY total_death_count desc
-
--- Global Numbers
 SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)* 100 as death_percentage
 FROM PortfolioProject..CovidDeaths
 WHERE continent is not null
 --GROUP BY date
 ORDER BY 1,2
 
+-- 2. Continents with the highest death count per population
 
--- Join CovidDeaths table and CovidVaccinations table
--- Looking at Total Population vs Vaccinations
-WITH PopvsVac (continent, location, date, population, new_vaccinations, rolling_vaccination_count)
-as
-(
-SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location ORDER BY dea.location,
-  dea.date) as rolling_vaccination_count
-FROM PortfolioProject..CovidDeaths dea
-JOIN PortfolioProject..CovidVaccinations vac
-	ON dea.location = vac.location
-	and dea.date = vac.date
--- WHERE dea.continent is not null
--- ORDER BY 2,3
-)
-SELECT *, (rolling_vaccination_count/population)*100 as percent_rolling_vaccination
-FROM PopvsVac
+SELECT continent, MAX(cast(total_deaths as int)) as total_death_count
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+GROUP BY continent
+ORDER BY total_death_count desc
+
+-- 3. Countries with highest infection rate compared to population
+
+SELECT location, population, MAX(total_cases) as highest_infection_count, MAX((total_cases/population))*100 as percent_population_infected
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+GROUP BY location, population
+ORDER BY percent_population_infected desc
+
+-- 4. Grouping the percent of population infected by Covid by date
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
 
 
--- TEMP TABLE
-DROP TABLE IF EXISTS #PercentPopulationVaccinated
+-- 5. Joining CovidDeaths table and CovidVaccinations table
+-- Creating a Temp Table showing rolling percent count of individuals vaccinated by country, location, and date
+
 CREATE TABLE #PercentPopulationVaccinated
 (
 continent nvarchar(255),
@@ -94,7 +62,7 @@ SELECT *, (rolling_vaccination_count/population)*100 as percent_rolling_vaccinat
 FROM #PercentPopulationVaccinated
 
 
--- Creating view to store data for later visualizations
+-- Creating view to store data for visualizing the rolling percent of population vaccinated by continent, location, and date
 
 Create View PercentPopulationVaccinated as
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
@@ -107,5 +75,3 @@ JOIN PortfolioProject..CovidVaccinations vac
 WHERE dea.continent is not null
 --ORDER BY 2,3
 
-SELECT *
-FROM PercentPopulationVaccinated
